fbf418c31c0f050dc1a5d211de0fc6c5c045cee5
ElenaK <ekroener@uni-potsdam.de>
2021-02-08 17:59:43 +0100

    Create template for class StringMatching

---
+++ stringmatching.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Elena Kröner
+
+
+class StringMatching:
+    """Find position of a string in another sting, text or multiple texts"""
+
+    def __init__(self, string, text, method="kmp"):
+        self.string = string
+        self.text = text
+        self.position = int()
+
+    def naive(self):
+        pass
+
+    def _prefix(self):
+        pass
+
+    def kmp_matcher(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
