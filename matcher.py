5fdbdeaeb91e2f32e521673b3115efbc8369afae
Elena <elena-kroener@web.de>
2021-03-09 15:54:06 +0100

    Restructure files: read_files now in Stringmatching class instead of matcher.py

--- matcher.py
+++ matcher.py
@@ -3,43 +3,9 @@
 # Python 3.8
 
 import argparse
-import os
 import sys
 
 import searchalgorithm as sm
-
-
-def readfiles(t):
-    """
-    Determine type of variable 'text' and turn into searchable list of string
-
-    Parameters
-    ----------
-    t : string, .txt-file or foulder
-        Text that will be transformed based on its type.
-
-    Returns
-    -------
-    texts : list of string or list of lists
-        List with text that will be searched in, either a string or multiple
-            sublists if input is a foulder with multiple .txt-files.
-
-    """
-    texts = []
-    if os.path.isfile(t):
-        with open(t, "r", encoding="utf-8") as t:
-            text = t.read()
-            texts.append(text)
-    elif os.path.isdir(t):
-        for root, dirs, files in os.walk(sys.argv[-1]):
-            for filename in files:
-                with open(root+"\\"+filename, "r", encoding="utf-8") as t:
-                    text = t.read()
-                    texts.append(text)
-    else:
-        text = sys.argv[-1]
-        texts.append(text)
-    return texts
 
 
 def define_argparser():
@@ -61,31 +27,29 @@ def main():
     args = define_argparser()
 
     # check if string is not empty and transform it with readfiles-function
-    string = sys.argv[-2]
+    string = args.suchbegriff
     if not string:
         print("Bitte einen Suchbegriff der Länge eins oder mehr eingeben.")
         sys.exit()
-    t = sys.argv[-1]
-    texts = readfiles(t)
+    text = args.text
 
-    for text in texts:
-        # choose search algorithm accoring to input arguments
-        if args.ignore and args.naive:
-            m = sm.StringMatching(string, text, case="ignore", method="naive")
-            print("Ignoriere Groß- und Kleinschreibung, suche mit naivem Algorithmus")
-        elif args.ignore:
-            m = sm.StringMatching(string, text, case="ignore")
-            print("Ignoriere Groß- und Kleinschreibung")
-        elif args.naive:
-            print("Verwende naiven Algorithmus")
-            m = sm.StringMatching(string, text, method="naive")
-        else:
-            m = sm.StringMatching(string, text)
-        position = m.matcher()
-        if not position:
-            print("Der Suchbegriff konnte nicht gefunden werden.")
-        else:
-            print(position)
+    # choose search algorithm accoring to input arguments
+    if args.ignore and args.naive:
+        m = sm.StringMatching(string, text, case="ignore", method="naive")
+        print("Ignoriere Groß- und Kleinschreibung, suche mit naivem Algorithmus")
+    elif args.ignore:
+        m = sm.StringMatching(string, text, case="ignore")
+        print("Ignoriere Groß- und Kleinschreibung")
+    elif args.naive:
+        print("Verwende naiven Algorithmus")
+        m = sm.StringMatching(string, text, method="naive")
+    else:
+        m = sm.StringMatching(string, text)
+    position = m.matcher()
+    if not position:
+        print("Der Suchbegriff konnte nicht gefunden werden.")
+    else:
+        print(position)
 
 
 if __name__ == "__main__":
--- searchalgorithm.py
+++ searchalgorithm.py
@@ -2,6 +2,8 @@
 # Elena Kröner
 # Python 3.8
 
+import os
+
 
 class StringMatching:
     """
@@ -23,11 +25,11 @@ class StringMatching:
 
     def __init__(self, string, text, method="kmp", case=""):
         if case == "ignore":
-            self.string = list(string.lower())
-            self.text = list(text.lower())
+            self.string = string.lower()
+            self.text = self.__read_files(text, case="ignore")
         else:
-            self.string = list(string)
-            self.text = list(text)
+            self.string = string
+            self.text = self.__read_files(text)
         if method in self.methods:
             self.method = method
         self.position = list()
@@ -38,15 +40,56 @@ class StringMatching:
         elif self.method == "kmp":
             return self.__kmp_matcher()
 
-    def __naive(self):
-        n = len(self.text)
-        m = len(self.string)
-        for s in range(n-m+1):
-            if self.string == self.text[s:s+m]:
-                self.position.append(s)
-        return ', '.join(map(str, self.position))
+    def __read_files(self, text, case=""):
+        """
+        Determine type of variable text and turn into searchable list of string
 
-    def __prefix(self):
+        Parameters
+        ----------
+        t : string, .txt-file or foulder
+            Text that will be transformed based on its type.
+        case : string, default=""
+            set to case-insensitive if case="ignore"
+
+        Returns
+        -------
+        texts : list of string or list of lists
+            List with text that will be searched in, either a string or
+            multiple sublists if input is a foulder with multiple .txt-files.
+        """
+        texts = []
+        if os.path.isfile(text):
+            with open(text, "r", encoding="utf-8") as t:
+                text = t.read()
+                texts.append(text)
+        elif os.path.isdir(text):
+            for root, dirs, files in os.walk(text):
+                for filename in files:
+                    with open(root+"\\"+filename, "r", encoding="utf-8") as t:
+                        text = t.read()
+                        texts.append(text)
+        else:
+            texts = [text]
+        if case == "ignore":
+            texts = [text.lower() for text in texts]
+        return texts
+
+    def __naive(self):
+        for t in self.text:
+            found = []
+            n = len(t)
+            m = len(self.string)
+            for s in range(n-m+1):
+                if self.string == t[s:s+m]:
+                    found.append(s)
+            self.position.append(found)
+        if len(self.position) > 1:
+            return ', '.join(map(str, self.position))
+        else:
+            return ', '.join(map(str, found))
+
+    @staticmethod
+    def __prefix(string):
         """
         Compare string with itself
 
@@ -56,13 +99,13 @@ class StringMatching:
             Index of string as key with number of repeated characters from
             beginning as value.
         """
-        m = len(self.string)
+        m = len(string)
         pi = {0: 0}
         k = 0
         for q in range(1, m):
-            while k > 0 and self.string[k] != self.string[q]:
+            while k > 0 and string[k] != string[q]:
                 k = pi[k]
-            if self.string[k] == self.string[q]:
+            if string[k] == string[q]:
                 k += 1
             pi[q] = k
         return pi
@@ -76,19 +119,25 @@ class StringMatching:
         list
             Contains all starting positions of search term in larger string.
         """
-        n = len(self.text)
-        m = len(self.string)
-        pi = self.__prefix()
-        q = 0
-        for i in range(n):
-            while q > 0 and self.string[q] != self.text[i]:
-                q = pi[q]
-            if self.string[q] == self.text[i]:
-                q += 1
-            if q == m:
-                self.position.append(i-m+1)
-                q = pi[q-1]
-        return ', '.join(map(str, self.position))
+        for t in self.text:
+            found = []
+            n = len(t)
+            m = len(self.string)
+            pi = self.__prefix(self.string)
+            q = 0
+            for i in range(n):
+                while q > 0 and self.string[q] != t[i]:
+                    q = pi[q]
+                if self.string[q] == t[i]:
+                    q += 1
+                if q == m:
+                    found.append(i-m+1)
+                    q = pi[q-1]
+            self.position.append(found)
+        if len(self.position) > 1:
+            return ', '.join(map(str, self.position))
+        else:
+            return ', '.join(map(str, found))
 
 
 def main():
@@ -117,6 +166,14 @@ def main():
     print("Beispiel für KMP und case-insentitve:\n"
           "bar in RhabarberBarbaraBarbarenBartBarbierBierBarBärbel",
           kmp_string_ignore.matcher())
+
+    kmp_file = StringMatching("tree", r"example\news.txt")
+    print("Beispiel für default Suche in einer Datei:\n"
+          "tree in news.txt:", kmp_file.matcher())
+
+    kmp_folder = StringMatching("Tree", r"example", case="ignore")
+    print("Beispiel für case-insensitive Suche in einem Ordner:\n"
+          "Tree im Ordner example:", kmp_folder.matcher())
 
 
 if __name__ == "__main__":
